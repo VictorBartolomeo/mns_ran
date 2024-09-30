12f7b3c06bb2137f0917f1dc450b849d967de789
VictorBartolomeo <victor.monteragioni@icloud.com>
2024-09-30 16:37:17 +0200

    Test ajout Steph

--- Python/ExercicesPy/Cours tuples.py
+++ Python/ExercicesPy/Cours tuples.py
@@ -2,12 +2,11 @@ from ctypes.wintypes import PUSHORT
 
 notes=(("franck",(5,14,8)),
     ("tom",(1,14,18)),
-    ("Dider", (12,18,20)),
+    ("Didier", (12,18,20)),
 )
 
 for personne in notes:
     note_personne=personne[1]
     moyenne=sum(note_personne)/len(note_personne)
     print(f"{personne[0]} a {moyenne:.2f} de moyenne")
-
 
